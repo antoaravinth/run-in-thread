2c5c9937504b7e485dd5f8b35b42a7609fa94ce7
abelginrayen <abelginrayen@paypal.com>
2018-01-30 19:15:07 +0530

    Initial checkins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
+var Worker = require('webworker-threads').Worker;
 
-export default function hello(user = 'World') {
-  return `Hello ${user}!\n`;
+
+function runInThread(asyncFunction) {
+
+    const fn = () => {
+        this.onmessage = (e) => {
+            console.log(JSON.stringify(e));
+            console.log("h");
+            // console.log(userFn)
+
+            /*
+            Promise.resolve(e.data[1]).then(
+                asyncFunction.apply.bind(asyncFunction, asyncFunction)
+            ).then(
+                d => {
+                    postMessage([e.data[0], 0, d]);
+                },
+                e => {
+                    postMessage([e.data[0], 1, ''+e]);
+                }
+            )*/
+
+            self.close();
+        }
+    };
+
+    let worker = new Worker(fn);
+
+    worker.onmessage = e => {
+      console.log(e)
+      console.log(JSON.stringify(e));
+    };
+
+    return function(args) {
+        args = [].slice.call(arguments);
+        return new Promise(() => {
+          worker.postMessage([0,args])
+        })
+    }
 }
 
-if (require.main === module) {
-  process.stdout.write(hello());
-}
+
+
+const getName = runInThread(async username => {
+    console.log("callled..")
+});
+
+getName('developit')
+
+/*
+const asyncFn = () => {
+    postMessage("I'm working before postMessage('ali').");
+    this.onmessage = function(event) {
+        postMessage('Hi ' + event.data);
+        self.close();
+    };
+};
+
+// You may also pass in a function:
+var worker = new Worker(asyncFn);
+
+
+worker.onmessage = function(event) {
+    console.log("Worker said : " + event.data);
+};
+worker.postMessage('ali');
+*/
